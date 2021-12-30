b61fa1f56c5e1ca742b99acdc6bc5514893b9d30
qinfeipeng <qinfeipeng@szyh-smart.com>
2021-12-30 10:20:56 +0800

    the first file

---
+++ task.c
@@ -0,0 +1,18 @@
+#ifdef GLOBAL
+	#define _EXT_
+#else
+	#define	_EXT_   extern
+#endif
+
+      _EXT_   char obu_channel
+
+      _EXT_   char obu_info
+
+      _EXT_   char obu_mac
+	  
+	  
+	  int _2ab;
+	  int 2ab_;
+	  
+	  int 2_ab;
+	  
